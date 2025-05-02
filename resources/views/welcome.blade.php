e64607f63ee134b7d5a4636aa920ec3efc3620d7
Mauricio <mauricio.araras@hotmail.com>
2025-05-02 15:20:10 -0300

    :heavy_plus_sign: Import and adjusts project images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,48 +16,6 @@
         name="keywords"
         content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard"
     />
-    <!--end::Primary Meta Tags-->
-    <!--begin::Fonts-->
-    {{--<link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
-        integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q="
-        crossorigin="anonymous"
-    />--}}
-    <!--end::Fonts-->
-    <!--begin::Third Party Plugin(OverlayScrollbars)-->
-    {{--<link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/styles/overlayscrollbars.min.css"
-        integrity="sha256-tZHrRjVqNSRyWg2wbppGnT833E/Ys0DHWGwT04GiqQg="
-        crossorigin="anonymous"
-    />--}}
-    <!--end::Third Party Plugin(OverlayScrollbars)-->
-    <!--begin::Third Party Plugin(Bootstrap Icons)-->
-    {{--<link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
-        integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI="
-        crossorigin="anonymous"
-    />--}}
-    <!--end::Third Party Plugin(Bootstrap Icons)-->
-    <!--begin::Required Plugin(AdminLTE)-->
-    {{--<link rel="stylesheet" href="../../dist/css/adminlte.css" />--}}
-    <!--end::Required Plugin(AdminLTE)-->
-    <!-- apexcharts -->
-    {{--<link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.css"
-        integrity="sha256-4MX+61mt9NVvvuPjUWdUdyfZfxSB1/Rf9WtqRHgG5S0="
-        crossorigin="anonymous"
-    />--}}
-    <!-- jsvectormap -->
-    {{--<link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/css/jsvectormap.min.css"
-        integrity="sha256-+uGLJmmTKOqBr+2E6KDYs/NRsHxSkONXFHUL0fy2O/4="
-        crossorigin="anonymous"
-    />--}}
 
     @vite('resources/scss/app.scss')
 </head>
@@ -102,7 +60,7 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0">
                                     <img
-                                        src="../../dist/assets/img/user1-128x128.jpg"
+                                        src="{{ Vite::asset('resources/images/user1-128x128.jpg') }}"
                                         alt="User Avatar"
                                         class="img-size-50 rounded-circle me-3"
                                     />
@@ -128,7 +86,7 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0">
                                     <img
-                                        src="../../dist/assets/img/user8-128x128.jpg"
+                                        src="{{ Vite::asset('resources/images/user8-128x128.jpg') }}"
                                         alt="User Avatar"
                                         class="img-size-50 rounded-circle me-3"
                                     />
@@ -154,7 +112,7 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0">
                                     <img
-                                        src="../../dist/assets/img/user3-128x128.jpg"
+                                        src="{{ Vite::asset('resources/images/user3-128x128.jpg') }}"
                                         alt="User Avatar"
                                         class="img-size-50 rounded-circle me-3"
                                     />
@@ -219,7 +177,7 @@
                 <li class="nav-item dropdown user-menu">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                         <img
-                            src="../../dist/assets/img/user2-160x160.jpg"
+                            src="{{ Vite::asset('resources/images/user2-160x160.jpg') }}"
                             class="user-image rounded-circle shadow"
                             alt="User Image"
                         />
@@ -229,7 +187,7 @@
                         <!--begin::User Image-->
                         <li class="user-header text-bg-primary">
                             <img
-                                src="../../dist/assets/img/user2-160x160.jpg"
+                                src="{{ Vite::asset('resources/images/user2-160x160.jpg') }}"
                                 class="rounded-circle shadow"
                                 alt="User Image"
                             />
@@ -273,7 +231,7 @@
             <a href="./index.html" class="brand-link">
                 <!--begin::Brand Image-->
                 <img
-                    src="../../dist/assets/img/AdminLTELogo.png"
+                    src="{{ Vite::asset('resources/images/AdminLTELogo.png') }}"
                     alt="AdminLTE Logo"
                     class="brand-image opacity-75 shadow"
                 />
@@ -908,7 +866,7 @@
                                         <!-- /.direct-chat-infos -->
                                         <img
                                             class="direct-chat-img"
-                                            src="../../dist/assets/img/user1-128x128.jpg"
+                                            src="{{ Vite::asset('resources/images/user1-128x128.jpg') }}"
                                             alt="message user image"
                                         />
                                         <!-- /.direct-chat-img -->
@@ -927,7 +885,7 @@
                                         <!-- /.direct-chat-infos -->
                                         <img
                                             class="direct-chat-img"
-                                            src="../../dist/assets/img/user3-128x128.jpg"
+                                            src="{{ Vite::asset('resources/images/user3-128x128.jpg') }}"
                                             alt="message user image"
                                         />
                                         <!-- /.direct-chat-img -->
@@ -944,7 +902,7 @@
                                         <!-- /.direct-chat-infos -->
                                         <img
                                             class="direct-chat-img"
-                                            src="../../dist/assets/img/user1-128x128.jpg"
+                                            src="{{ Vite::asset('resources/images/user1-128x128.jpg') }}"
                                             alt="message user image"
                                         />
                                         <!-- /.direct-chat-img -->
@@ -963,7 +921,7 @@
                                         <!-- /.direct-chat-infos -->
                                         <img
                                             class="direct-chat-img"
-                                            src="../../dist/assets/img/user3-128x128.jpg"
+                                            src="{{ Vite::asset('resources/images/user3-128x128.jpg') }}"
                                             alt="message user image"
                                         />
                                         <!-- /.direct-chat-img -->
@@ -1171,29 +1129,7 @@
     </footer>
     <!--end::Footer-->
 </div>
-<!--end::App Wrapper-->
-<!--begin::Script-->
-<!--begin::Third Party Plugin(OverlayScrollbars)-->
-{{--<script
-    src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
-    integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
-    crossorigin="anonymous"
-></script>
-<!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-<script
-    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"
-></script>
-<!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
-<script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-    crossorigin="anonymous"
-></script>
-<!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-<script src="../../dist/js/adminlte.js"></script>--}}
-<!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+
 <script>
     const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
     const Default = {
@@ -1214,15 +1150,7 @@
         }
     });
 </script>
-<!--end::OverlayScrollbars Configure-->
-<!-- OPTIONAL SCRIPTS -->
-<!-- sortablejs -->
-{{--<script
-    src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"
-    integrity="sha256-ipiJrswvAR4VAx/th+6zWsdeYmVae0iJuiR+6OqHJHQ="
-    crossorigin="anonymous"
-></script>--}}
-<!-- sortablejs -->
+
 <script>
     const connectedSortables = document.querySelectorAll('.connectedSortable');
     connectedSortables.forEach((connectedSortable) => {
@@ -1237,13 +1165,7 @@
         cardHeader.style.cursor = 'move';
     });
 </script>
-<!-- apexcharts -->
-{{--<script
-    src="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.min.js"
-    integrity="sha256-+vh8GkaU7C9/wbSLIcwq82tQ2wTf44aOHA8HlBMwRI8="
-    crossorigin="anonymous"
-></script>--}}
-<!-- ChartJS -->
+
 <script>
     // NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
     // IT'S ALL JUST JUNK FOR DEMO
@@ -1302,18 +1224,7 @@
     );
     sales_chart.render();
 </script>
-<!-- jsvectormap -->
-{{--<script
-    src="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/js/jsvectormap.min.js"
-    integrity="sha256-/t1nN2956BT869E6H4V1dnt0X5pAQHPytli+1nTZm2Y="
-    crossorigin="anonymous"
-></script>
-<script
-    src="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/maps/world.js"
-    integrity="sha256-XPpPaZlU8S/HWf7FZLAncLg2SAkP8ScUTII89x9D3lY="
-    crossorigin="anonymous"
-></script>--}}
-<!-- jsvectormap -->
+
 <script>
     const visitorsData = {
         US: 398, // USA
